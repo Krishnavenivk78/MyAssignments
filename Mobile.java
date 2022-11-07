092dea222a631381510c5bac664526f93fe7f340
Krishnavenivk78 <117562884+Krishnavenivk78@users.noreply.github.com>
2022-11-07 06:06:26 +0530

    Add files via upload

---
+++ Mobile.java
@@ -0,0 +1,38 @@
+package week1.day1;
+
+public class Mobile {
+
+		public static void main(String[] args)
+		{
+			Mobile mymobile = new Mobile();
+			
+			//Declaring the Variables
+			
+			String mobileBrandName = "Nokia";
+			char mobileLogo = 'N';
+			short noOfMobilePiece = 50;
+			int modelNumber = 12345;
+			long mobileImeiNumber = 123456789;
+			float mobilePrice = 69999;
+			boolean isDamaged = false;
+			
+			System.out.println("Welcome to My First Test Leaf Java Assignment on Mobliles");
+			System.out.println("Mobile Brand: " +mobileBrandName);
+			System.out.println("Mobile Logo: " +mobileLogo);
+			System.out.println("Mobile Price: " +noOfMobilePiece);
+			System.out.println("Model Number: " +modelNumber);
+			System.out.println("Mobile MEI Number: " +mobileImeiNumber);
+			System.out.println("Mobile Price " +mobilePrice);
+			System.out.println("Is the Mobile Damaged: " +isDamaged);
+			
+		}
+		
+		
+		
+		
+	}
+
+
+
+
+
---
+++ TwoWheeler.java
@@ -0,0 +1,34 @@
+package week1.day1;
+
+public class TwoWheeler {
+
+	public static void main(String[] args)
+	{
+		
+		TwoWheeler mybike = new TwoWheeler();
+		
+		//Declaring the Variables
+		
+		int noofWheels = 2;
+		short noOfMirrors = 2;
+		long chassisNumber = 55555;
+		boolean isPunctured = false;
+		String bikeName = "Hero Honda";
+		double runningKM = 5000.1234565;
+		
+		System.out.println("Welcome to My First Test Leaf Java Assignment on Two Wheelers");
+		System.out.println("Bike Name: " +bikeName);
+		System.out.println("Number of Wheels: " +noofWheels);
+		System.out.println("Number of Mirros: " +noOfMirrors);
+		System.out.println("Chassis Number: " +chassisNumber);
+		System.out.println("Is the Bike Punctured: " +isPunctured);
+		System.out.println("Bike's Running Kilometers: " +runningKM);
+	}
+	
+	
+	
+	
+}
+
+
+
